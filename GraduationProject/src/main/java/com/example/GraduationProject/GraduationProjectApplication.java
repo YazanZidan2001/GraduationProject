38db83143574e268c4b12d8308b06cac24a0269c
yazan zidan <yazoonzidan@gmail.com>
2024-09-06 15:29:25 +0300

    test

--- GraduationProject/src/main/java/com/example/GraduationProject/GraduationProjectApplication.java
+++ GraduationProject/src/main/java/com/example/GraduationProject/GraduationProjectApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GraduationProjectApplication {
+	//tets
 
 	public static void main(String[] args) {
 		SpringApplication.run(GraduationProjectApplication.class, args);
