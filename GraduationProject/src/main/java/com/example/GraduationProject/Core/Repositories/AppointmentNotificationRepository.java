720d26ee455d74f8cf9db9b637c45c2416ac3f8a
yazan zidan <yazoonzidan@gmail.com>
2025-02-15 03:39:58 +0200

    Update AppointmentNotificationRepository.java

--- GraduationProject/src/main/java/com/example/GraduationProject/Core/Repositories/AppointmentNotificationRepository.java
+++ GraduationProject/src/main/java/com/example/GraduationProject/Core/Repositories/AppointmentNotificationRepository.java
@@ -3,6 +3,7 @@ package com.example.GraduationProject.Core.Repositories;
 import com.example.GraduationProject.Common.CompositeKey.AppointmentCompositeKey;
 import com.example.GraduationProject.Common.Entities.AppointmentNotification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -15,6 +16,18 @@ public interface AppointmentNotificationRepository extends JpaRepository<Appoint
     // Find notifications that need to be sent (date and time is 24 hours from now)
     List<AppointmentNotification> findByDateAndTimeAndIsSendFalse(LocalDate date, String time);
 
+    @Query("""
+        SELECT an FROM AppointmentNotification an 
+        JOIN Appointment a 
+        ON an.appointmentID = a.appointmentID
+        AND an.doctorID = a.doctorID
+        AND an.patientID = a.patientID
+        AND an.clinicID = a.clinicID
+        WHERE an.patientID = :patientID 
+        AND an.isSend = false 
+        AND a.isDone = false 
+        AND a.isCancelled = false
+    """)
     List<AppointmentNotification> findByPatientIDAndIsSendFalse(Long patientID);
 
     Optional<AppointmentNotification> findByAppointmentID(Long appointmentID);
